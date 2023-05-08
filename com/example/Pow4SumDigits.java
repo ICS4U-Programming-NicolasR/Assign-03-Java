586b541f2359b85b3ec96b700de4f01b7d1b89e5
Nicolas Riscalas <nicolas.riscalas@stu.ocsb.ca>
2023-05-08 00:26:52 +0000

    created the program added comments and style checked

--- com/example/Pow4SumDigits.java
+++ com/example/Pow4SumDigits.java
@@ -4,17 +4,16 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Scanner;
 
 /**
- * Binary search function.
+ * Assignment 3.
  *
  * @author Mr. Riscalas
  * @version 1.0
- * @since 2023-04-24
+ * @since 2023-05-07
  */
-public final class pow4SumDigits {
+public final class Pow4SumDigits {
     /**
      * This is a private constructor used to satisfy the
      * style checker.
@@ -22,27 +21,30 @@ public final class pow4SumDigits {
      * @exception IllegalStateException Utility class.
      * @see IllegalStateException
      */
-    private pow4SumDigits() {
+    private Pow4SumDigits() {
         throw new IllegalStateException("Utility class");
     }
 
     /**
-    * This is the recBinSearch method.
+    * This is the sumOfDigits method.
     *
     * @param index // the index
     * @param stringNums // String array of Nums
     * @return sum
     */
     public static int sumOfDigits(final int index, String[] stringNums) {
+        // base case
         if (index >= stringNums.length) {
             return 0;
         } else {
-            return sumOfDigits(index + 1, stringNums) + Integer.parseInt(stringNums[index]);
+            // recursive sum of return statement
+            return sumOfDigits(index + 1, stringNums)
+                + Integer.parseInt(stringNums[index]);
         }
     }
 
     /**
-    * This is the recBinSearch method.
+    * This is the sumOfDigits method.
     *
     * @param num //base
     * @return sum
@@ -53,31 +55,35 @@ public final class pow4SumDigits {
     }
 
     /**
-    * This is the recBinSearch method.
+    * This is the powerOfFour method.
     *
-    * @param index // the index
-    * @param stringNums // String array of Nums
-    * @return sum
+    * @param num // the number
+    * @param multipleOfFour // the different powers of 4
+    * @return boolean
     */
     public static boolean powerOfFour(final int num, final int multipleOfFour) {
+        // base case
+        final int four = 4;
         if (multipleOfFour == num) {
             return true;
         } else if (multipleOfFour > num) {
             return false;
         } else {
-            return powerOfFour(num, multipleOfFour * 4)
+            // calculate the next power of 4
+            return powerOfFour(num, multipleOfFour * four);
         }
     }
 
     /**
-    * This is the recBinSearch method.
+    * This is the powerOfFour method.
     *
     * @param num //base
-    * @return index
+    * @return boolean
     */
     public static boolean powerOfFour(final int num) {
+        final int four = 4;
         // call the same function but this time with more arguments
-        return powerOfFour(num, 4);
+        return powerOfFour(num, four);
     }
 
     /**
@@ -92,20 +98,31 @@ public final class pow4SumDigits {
         // Read input from file using Scanner
         try (Scanner inputFile = new Scanner(new File(inputFilePath));
             FileWriter writer = new FileWriter(outputFilePath)) {
-            /* Read each line from the input file and find using a binary search
-            * the index of the random numbers using recursions
+            /* Read each line from the input file and convert to integer then
+            * call both the power of four function and the sum of digits one
             */
             while (inputFile.hasNextLine()) {
                 try {
-                    final int number = Integer.parseInt(inputFile.nextLine().strip());
-                    // find the index by calling the recBinSearch
+                    // strip the string and convert to int
+                    final int number = Integer.parseInt(inputFile.nextLine()
+                                                    .strip());
+                    // calculate the sum of digits
                     final int sum = sumOfDigits(number);
+                    // find out if it is a power of four
                     final boolean powerOfFour = powerOfFour(number);
                     // Write the answer to the output file
                     writer.write("All the digits of your num added up is: "
-                            + sum);
-                } catch (NumberFormatException error){
-                    System.err.println("Incorrect input: " + error.getMessage());
+                            + sum + "\n");
+                    // if it is a power of four write that otherwise write that
+                    // it isn't
+                    if (powerOfFour) {
+                        writer.write(number + " is a power of 4\n");
+                    } else {
+                        writer.write(number + " is not a power of 4\n");
+                    }
+                } catch (NumberFormatException error) {
+                    System.err.println("Incorrect input: "
+                        + error.getMessage());
                 }
             }
         } catch (FileNotFoundException error) {
